24286aa15a85b738c7d1af7cfd8b0e771a8a10e9
Fantasy Shao <forever.fantasy27@gmail.com>
2014-10-09 16:12:36 +0800

    Add UIRefreshControl.

--- README.md
+++ README.md
@@ -7,13 +7,18 @@ ZhihuDaily iOS client in Swift
 
 Simple ZhihuDaily client written in Swift built with Xcode6.
 
+### Screenshot
+
+![]()
+![]()
+
 ### Dependence
 
 - [Alamofire](https://github.com/Alamofire/Alamofire): Elegant HTTP Networking in Swift
 
 ## Todo
 
-- [ ] Add `UIRefreshControl`
+- [x] Add `UIRefreshControl`
 - [ ] Customize the UI of TableView
 - [ ] Add navigation in detailView
 - [ ] Customize the UI of detailView
--- ZhihuDaily/ViewController.swift
+++ ZhihuDaily/ViewController.swift
@@ -11,22 +11,71 @@ import Alamofire
 
 class ViewController: UIViewController, UITableViewDelegate {
 
-    @IBOutlet var tableView: UITableView!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var navBar: UINavigationBar!
+    @IBOutlet weak var navItem: UINavigationItem!
     
+    var latestItems = NSMutableArray()
     let url = "http://news-at.zhihu.com/api/3/news/latest"
     let identifier = "cell"
     
-    var latestItems = NSMutableArray()
+    // UI related params
+    let appTitle: String = "知乎 · 日报"
+    let navBgColor: UIColor = UIColor(red: 0.3, green: 0.4, blue: 0.9, alpha: 0)
+    var refreshControl = UIRefreshControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.title = "知乎 · 日报"
-        self.view.backgroundColor = UIColor.whiteColor()
-        
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.identifier)
         
+        self.configUI()
         self.loadData()
+    }
+    
+    // Customize UI
+    func configUI() {
+        
+        // View
+        self.title = self.appTitle
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+        // Refresh
+        self.refreshControl.addTarget(self, action: "loadData", forControlEvents: .ValueChanged)
+        self.refreshControl.attributedTitle = NSAttributedString(string: "下拉刷新")
+        self.tableView.insertSubview(refreshControl, atIndex: 0)
+        
+        // Nav bar
+        self.navBar.tintColor = self.navBgColor
+        self.navBar.barTintColor = self.navBgColor
+        self.navBar.backgroundColor = self.navBgColor
+        
+        // Nav bar item
+        self.navItem.title = self.appTitle
+        
+    }
+    
+    // Load data
+    func loadData() {
+        Alamofire.request(.GET, self.url).responseJSON {
+            (req, res, data, err) in
+            // Data load fail
+            if err != nil {
+                // Reference: http://stackoverflow.com/questions/24022479/how-would-i-create-a-uialertview-in-swift
+                var alert = UIAlertController(title: "Zhihu Daily", message: "Data load failed", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            } else {
+                var arr = data?["stories"] as NSArray
+                
+                for data : AnyObject in arr {
+                    self.latestItems.addObject(data)
+                }
+                
+                self.tableView!.reloadData()
+                self.refreshControl.endRefreshing()
+            }
+        }
     }
     
     func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
@@ -57,27 +106,6 @@ class ViewController: UIViewController, UITableViewDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    func loadData() {
-        Alamofire.request(.GET, self.url).responseJSON {
-            (req, res, data, err) in
-            // 数据加载失败
-            if err != nil {
-                // Reference: http://stackoverflow.com/questions/24022479/how-would-i-create-a-uialertview-in-swift
-                var alert = UIAlertController(title: "Zhihu Daily", message: "Data load failed", preferredStyle: UIAlertControllerStyle.Alert)
-                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-                self.presentViewController(alert, animated: true, completion: nil)
-            } else {
-                var arr = data?["stories"] as NSArray
-                
-                for data : AnyObject in arr {
-                    self.latestItems.addObject(data)
-                }
-                
-                self.tableView!.reloadData()
-            }
-        }
-    }
-    
+
 }
 
